dc627764617b52869250fee77f92707b821371da
Oleksii Rudenko <rudenko2911@gmail.com>
2026-09-19 22:16:15 +0300

    Implement button-triggered LED interaction
    The previous time-based LED pattern is replaced with interaction via a button. Pressing the button now briefly illuminates the indicators. This establishes the foundation for more interactive modes.

--- hw_1.4/src/main.cpp
+++ hw_1.4/src/main.cpp
@@ -1,41 +1,48 @@
 #include <Arduino.h>
-#include <TimeLib.h>
+// #include <TimeLib.h>
 
 constexpr int LED_BLUE_PIN = 15;
 constexpr int LED_RED_PIN = 16;
 constexpr int BUTTON_PIN = 21;
+constexpr int DEBOUNCE_MILLIS = 50;
 
-int lastSecond = -1;
+unsigned long lastActionMillis = 0;
+unsigned int currentButtonState = HIGH;
+unsigned int lastButtonState = HIGH;
 
 void setup() {
-    pinMode(LED_BLUE_PIN, OUTPUT);
-    pinMode(LED_RED_PIN, OUTPUT);
     Serial.begin(115200);
     delay(1000);
+
+    pinMode(LED_BLUE_PIN, OUTPUT);
+    pinMode(LED_RED_PIN, OUTPUT);
+    pinMode(BUTTON_PIN, INPUT_PULLUP);
 }
 
-void mode1();
+static void mode1();
 
 void loop() {
     mode1();
 }
 
-void mode1() {
-    int currentSecond = second();
-    if (currentSecond != lastSecond) {
-        lastSecond = currentSecond;
-        if (currentSecond % 2 == 0) {
+static void mode1() {
+    currentButtonState = digitalRead(BUTTON_PIN);
+    unsigned long currentMillis = millis();
+
+    if (currentButtonState != lastButtonState && currentMillis - lastActionMillis >= DEBOUNCE_MILLIS) {
+        lastActionMillis = currentMillis;
+        lastButtonState = currentButtonState;
+
+        Serial.print("currentButtonState is ");
+        Serial.print(currentButtonState);
+        Serial.print("\n");
+
+        if (currentButtonState == LOW) {
             digitalWrite(LED_BLUE_PIN, HIGH);
-            digitalWrite(LED_RED_PIN, LOW);
-            Serial.print("Second: ");
-            Serial.print(currentSecond);
-            Serial.println(" - LED ON");
-        } else {
-            digitalWrite(LED_BLUE_PIN, LOW);
             digitalWrite(LED_RED_PIN, HIGH);
-            Serial.print("Second: ");
-            Serial.print(currentSecond);
-            Serial.println(" - LED OFF");
+            delay(200);
+            digitalWrite(LED_BLUE_PIN, LOW);
+            digitalWrite(LED_RED_PIN, LOW);
         }
     }
 }
