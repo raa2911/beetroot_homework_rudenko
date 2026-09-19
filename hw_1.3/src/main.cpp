527193925c6942ee425471c7a40a74e78a2c6561
Oleksii Rudenko <rudenko2911@gmail.com>
2026-09-19 19:50:36 +0300

    Revert "Simplify LED control to a single indicator"
    This reverts commit 6861923654083bffd43caf1a8c0fc569984723b2.

--- hw_1.3/src/main.cpp
+++ hw_1.3/src/main.cpp
@@ -1,12 +1,14 @@
 #include <Arduino.h>
 #include <TimeLib.h>
 
-constexpr int LED_PIN = 10;
+constexpr int LED_BLUE = 42;
+constexpr int LED_RED = 20;
 
 int lastSecond = -1;
 
 void setup() {
-    pinMode(LED_PIN, OUTPUT);
+    pinMode(LED_BLUE, OUTPUT);
+    pinMode(LED_RED, OUTPUT);
     Serial.begin(115200);
 }
 
@@ -15,12 +17,14 @@ void loop() {
     if (currentSecond != lastSecond) {
         lastSecond = currentSecond;
         if (currentSecond % 2 == 0) {
-            digitalWrite(LED_PIN, HIGH);
+            digitalWrite(LED_BLUE, HIGH);
+            digitalWrite(LED_RED, LOW);
             Serial.print("Second: ");
             Serial.print(currentSecond);
             Serial.println(" - LED ON");
         } else {
-            digitalWrite(LED_PIN, LOW);
+            digitalWrite(LED_BLUE, LOW);
+            digitalWrite(LED_RED, HIGH);
             Serial.print("Second: ");
             Serial.print(currentSecond);
             Serial.println(" - LED OFF");
