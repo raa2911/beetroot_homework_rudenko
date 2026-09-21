9ea4f4c874f5b2d64f7d866c19d4723445533744
Oleksii Rudenko <rudenko2911@gmail.com>
2026-09-21 20:44:14 +0300

    revert changes in 1.3

--- hw_1.3/src/main.cpp
+++ hw_1.3/src/main.cpp
@@ -1,37 +1,30 @@
 #include <Arduino.h>
 #include <TimeLib.h>
 
-constexpr int LED_BLUE_PIN = 15;
-constexpr int LED_RED_PIN = 16;
-constexpr int BUTTON_PIN = 16;
+constexpr int LED_BLUE = 42;
+constexpr int LED_RED = 20;
 
 int lastSecond = -1;
 
 void setup() {
-    pinMode(LED_BLUE_PIN, OUTPUT);
-    pinMode(LED_RED_PIN, OUTPUT);
+    pinMode(LED_BLUE, OUTPUT);
+    pinMode(LED_RED, OUTPUT);
     Serial.begin(115200);
 }
 
-void mode1();
-
 void loop() {
-    mode1();
-}
-
-void mode1() {
     int currentSecond = second();
     if (currentSecond != lastSecond) {
         lastSecond = currentSecond;
         if (currentSecond % 2 == 0) {
-            digitalWrite(LED_BLUE_PIN, HIGH);
-            digitalWrite(LED_RED_PIN, LOW);
+            digitalWrite(LED_BLUE, HIGH);
+            digitalWrite(LED_RED, LOW);
             Serial.print("Second: ");
             Serial.print(currentSecond);
             Serial.println(" - LED ON");
         } else {
-            digitalWrite(LED_BLUE_PIN, LOW);
-            digitalWrite(LED_RED_PIN, HIGH);
+            digitalWrite(LED_BLUE, LOW);
+            digitalWrite(LED_RED, HIGH);
             Serial.print("Second: ");
             Serial.print(currentSecond);
             Serial.println(" - LED OFF");
